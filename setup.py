d94965f9542c016594c6bbf6c36b0474256dfb81
Aria Ghora Prabono <hello@ghora.net>
2022-02-12 22:50:07 +0700

    Refactor

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="colabond",
+    version="0.1.0",
+    author="Aria Ghora Prabono",
+    author_email="hello@ghora.net",
+    description="Colabond command-line tool",
+    license="MIT",
+    scripts=["colabond"],
+)
