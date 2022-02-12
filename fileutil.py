94a5dcd58a823ab89bfe4c67c2d56b67c901bad2
Aria Ghora Prabono <hello@ghora.net>
2022-02-12 22:04:11 +0700

    Initial commit

---
+++ fileutil.py
@@ -0,0 +1,29 @@
+import os
+
+
+def scan_current_file_state():
+    project_file_state = dict()
+    # scan current directory recursively
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            # get only root directory from root
+            root_parts = root.split("/")
+
+            if len(root_parts) > 1:
+                root_dir = root_parts[1]
+                if root_dir in [
+                    ".colabond",
+                    ".git",
+                    ".idea",
+                    ".ipynb_checkpoints",
+                    ".node_modules",
+                    ".vscode",
+                    ".vscode-test",
+                    "__pycache__",
+                ]:
+                    continue
+
+            filename = os.path.join(root, file)
+            filedatetime = os.path.getmtime(filename)
+            project_file_state[filename] = filedatetime
+    return project_file_state
